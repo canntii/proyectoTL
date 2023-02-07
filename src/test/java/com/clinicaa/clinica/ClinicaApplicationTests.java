62a240a73fa23b84749ae375c80ab46a75b4af62
Bryan Cantillo <bryandc681@gmail.com>
2023-02-07 00:25:14 -0600

    tarea semana3 lista

---
+++ src/test/java/com/clinicaa/clinica/ClinicaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clinicaa.clinica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
